938bba3fe3f1df62eac310337feee8a8d4ff1f27
Joel Kirchartz <JKirchartz@gmail.com>
2013-05-02 13:08:51 -0400

    add slate to make.sh

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vimrc vim tmux.conf"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile vimrc vim tmux.conf slate"    # list of files/folders to symlink in homedir
 
 ##########
 
