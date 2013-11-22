7b3beb5b4c65832f85037f27e1878a1694757f6c
Joel Kirchartz <JKirchartz@gmail.com>
2013-11-22 16:04:53 -0500

    add ssh, screenrc, and gitconfig to make.sh

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vimrc vim tmux.conf slate"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile vimrc vim tmux.conf slate ssh gitconfig screenrc"    # list of files/folders to symlink in homedir
 
 ##########
 
