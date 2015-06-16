d617f8650e526da01c7e65441b2d7d6c2d8c43b3
Joel Kirchartz <jkirchartz@gmail.com>
2015-06-16 00:00:33 -0400

    cowsay tweaaks

--- scripts/cowsay.sh
+++ scripts/cowsay.sh
@@ -3,12 +3,15 @@
 # cowsay.sh
 # modified from github.com/msankhala/cowsayfun
 
-# automatically detect cowfile dir
-cowfiledir=( $(locate "*.cow" | rev | cut -d/ -f2- | rev | uniq))
+if [[ command -v locate 2>/dev/null ]]; then
+  # automatically detect cowfile dir w/ locate
+  cowfiledir=$(locate "*.cow" | rev | cut -d/ -f2- | rev | uniq)
+else
+  cowfiledir='/usr/share/cowsay/cows/'
+fi
 # #array of cow files
-files=( $(find $cowfiledir -maxdepth 1 -type f) )
-randomfileindex=$((RANDOM%${#files[@]}+0))
-randomcowfile=${files[$randomfileindex]}
+files=$(find $cowfiledir -maxdepth 1 -type f)
+randomcowfile=${files[RANDOM%${#files[@]}]}
 # warning: -a returns all fortunes, offensive or not;
 fortune -as | cowsay -f $randomcowfile
 
