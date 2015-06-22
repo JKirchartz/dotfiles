f57de741aa38a223aca239da9587e73bdbc32f08
Joel Kirchartz <jkirchartz@gmail.com>
2015-06-22 10:04:58 -0400

    fix cowsay.sh

--- scripts/cowsay.sh
+++ scripts/cowsay.sh
@@ -3,15 +3,16 @@
 # cowsay.sh
 # modified from github.com/msankhala/cowsayfun
 
-if [[ command -v locate 2>/dev/null ]]; then
-  # automatically detect cowfile dir w/ locate
-  cowfiledir=$(locate "*.cow" | rev | cut -d/ -f2- | rev | uniq)
-else
+if [[ -d '/usr/share/cowsay/cows/' ]]; then
   cowfiledir='/usr/share/cowsay/cows/'
+else
+  if command -v locate >/dev/null 2 >&1; then
+    # automatically detect cowfile dir w/ locate
+    cowfiledir=$(locate "*.cow" | rev | cut -d/ -f2- | rev | uniq)
+  fi
 fi
 # #array of cow files
-files=$(find $cowfiledir -maxdepth 1 -type f)
+files=( $(find $cowfiledir -maxdepth 1 -type f) )
 randomcowfile=${files[RANDOM%${#files[@]}]}
 # warning: -a returns all fortunes, offensive or not;
-fortune -as | cowsay -f $randomcowfile
-
+fortune -as | cowsay -f "$randomcowfile"
