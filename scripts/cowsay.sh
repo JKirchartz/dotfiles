0e1596e542a77cb64aff7ca2b24f249c86cd4e98
Joel Kirchartz <jkirchartz@gmail.com>
2015-06-22 10:24:58 -0400

    fix cowsay more

--- scripts/cowsay.sh
+++ scripts/cowsay.sh
@@ -9,10 +9,17 @@ else
   if command -v locate >/dev/null 2 >&1; then
     # automatically detect cowfile dir w/ locate
     cowfiledir=$(locate "*.cow" | rev | cut -d/ -f2- | rev | uniq)
+    if [[! -d $cowfiledir ]]; then
+      echo "Could not locate cow files, $cowfiledir is not a directory"
+    fi
   fi
 fi
 # #array of cow files
 files=( $(find $cowfiledir -maxdepth 1 -type f) )
 randomcowfile=${files[RANDOM%${#files[@]}]}
 # warning: -a returns all fortunes, offensive or not;
-fortune -as | cowsay -f "$randomcowfile"
+if [[ -f $randomcowfile ]]; then
+  fortune -as | cowsay -f "$randomcowfile"
+else
+  fortune -as | cowsay
+fi
