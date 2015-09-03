b5beff4cf7ac452003d9d45ae315eef93277418e
Joel Kirchartz <jkirchartz@gmail.com>
2015-09-03 11:15:18 -0400

    modify cowsay.sh, add wasabi256 vim colorscheme

--- scripts/cowsay.sh
+++ scripts/cowsay.sh
@@ -1,26 +1,15 @@
 #! /bin/bash
 #
 # cowsay.sh
-# modified from github.com/msankhala/cowsayfun
+# was modified from github.com/msankhala/cowsayfun, until I found a different 
+# way
 
-if [ -d '/usr/share/cowsay/cows/' ]; then
-  cowfiledir='/usr/share/cowsay/cows/'
+if type shuf > /dev/null; then
+  cowfile="$(cowsay -l | sed "1 d" | tr ' ' '\n' | shuf -n 1)"
 else
-  if command -v locate >/dev/null 2 >&1; then
-    # automatically detect cowfile dir w/ locate
-    cowfiledir=$(locate "*.cow" | rev | cut -d/ -f2- | rev | uniq)
-    if [! -d $cowfiledir ]; then
-      echo "Could not locate cow files, $cowfiledir is not a directory"
-    fi
-  fi
+  cowfiles=( $(cowsay -l | sed "1 d" | tr ' ' '\n') );
+  cowfile=${cowfiles[$(($RANDOM % ${#cowfiles[*]}))]}
 fi
-# #array of cow files
-files=$(find $cowfiledir -maxdepth 1 -type f)
-randomcowfile=${files[RANDOM % ${#files[@]}]}
-# warning: -a returns all fortunes, offensive or not;
-if [ -f "$randomcowfile" ]; then
-  fortune -as | cowsay -f "$randomcowfile"
-else
-  fortune -as | cowsay
-fi
+fortune -as | cowsay -f "$cowfile"
+
 
