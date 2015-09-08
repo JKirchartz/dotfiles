02662710490aef004d194b40f2ed80dec685a79c
Joel Kirchartz <jkirchartz@gmail.com>
2015-09-08 10:15:14 -0400

    fix cowsay script

--- scripts/cowsay.sh
+++ scripts/cowsay.sh
@@ -7,7 +7,7 @@
 if type shuf > /dev/null; then
   cowfile="$(cowsay -l | sed "1 d" | tr ' ' '\n' | shuf -n 1)"
 else
-  cowfiles=( $(cowsay -l | sed "1 d" | tr ' ' '\n') );
+  cowfiles=( $(cowsay -l | sed "1 d") );
   cowfile=${cowfiles[$(($RANDOM % ${#cowfiles[*]}))]}
 fi
 fortune -as | cowsay -f "$cowfile"
