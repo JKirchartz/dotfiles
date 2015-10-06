530ceee95f897032af677d4c730568598c6d9b5a
Joel Kirchartz <jkirchartz@gmail.com>
2015-10-06 16:01:24 -0400

    moar fun with fortune, update motd/wotd :facepunch:

---
+++ scripts/fortune.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#
+# fortune.sh
+# use my custom fortune file combined with whatever fortunes are on the system
+# give all fortune files equal respect (IDK why, egalitarianism?)
+# Copyleft (Ɔ) 2015 jkirchartz <jkirchartz@jkirchartz0.pit.corp.google.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+#
+#
+
+fortunedir=$(fortune -f 2>&1 | head -n1 | cut -d' ' -f2)
+
+fortune -ae ~/dotfiles/scripts/fortune $fortunedir
