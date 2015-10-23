2fb409217cb18665013a3e3089beeabf9d8d6e14
Joel Kirchartz <jkirchartz@gmail.com>
2015-10-23 10:43:14 -0400

    IDK anymore

---
+++ scripts/hack.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# hack.sh
+#
+# Copyleft (ↄ) 2015 jkirchartz <jkirchartz@jkirchartz0.pit.corp.google.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+
+
+./skull.sh
+
+./marquee hello world
---
+++ scripts/skull.sh
@@ -0,0 +1,106 @@
+#! /bin/sh
+#
+# skull.sh
+# get a random skull, in your command line!
+# Copyleft (ↄ) 2015 jkirchartz <jkirchartz@jkirchartz0.pit.corp.google.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+# Art from http://www.ascii-art.de/ascii/
+
+r=$(($RANDOM%4))
+case "$r" in
+"0")
+cat << "EOF"
+                            (
+                .            )        )
+                         (  (|              .
+                     )   )\/ ( ( (
+             *  (   ((  /     ))\))  (  )    )
+           (     \   )\(          |  ))( )  (|
+           >)     ))/   |          )/  \((  ) \
+           (     (      .        -.     V )/   )(    (
+            \   /     .   \            .       \))   ))
+              )(      (  | |   )            .    (  /
+             )(    ,'))     \ /          \( `.    )
+             (\>  ,'/__      ))            __`.  /
+            ( \   | /  ___   ( \/     ___   \ | ( (
+             \.)  |/  /   \__      __/   \   \|  ))
+            .  \. |>  \      | __ |      /   <|  /
+                 )/    \____/ :..: \____/     \ <
+          )   \ (|__  .      / ;: \          __| )  (
+         ((    )\)  ~--_     --  --      _--~    /  ))
+          \    (    |  ||               ||  |   (  /
+                \.  |  ||_             _||  |  /
+                  > :  |  ~V+-I_I_I-+V~  |  : (.
+                 (  \:  T\   _     _   /T  : ./
+                  \  :    T^T T-+-T T^T    ;<
+                   \..`_       -+-       _'  )
+         )            . `--=.._____..=--'. ./         (
+        ((     ) (          )             (     ) (   )>
+         > \/^/) )) (   ( /(.      ))     ))._/(__))./ (_.
+        (  _../ ( \))    )   \ (  / \.  ./ ||  ..__:|  _. \
+        |  \__.  ) |   (/  /: :)) |   \/   |(  <.._  )|  ) )
+       ))  _./   |  )  ))  __  <  | :(     :))   .//( :  : |
+       (: <     ):  --:   ^  \  )(   )\/:   /   /_/ ) :._) :
+        \..)   (_..  ..  :    :  : .(   \..:..    ./__.  ./
+                   ^    ^      \^ ^           ^\/^     ^ JaL
+EOF
+;;
+"1")
+cat << "EOF"
+                 uuuuuuu
+             uu$$$$$$$$$$$uu
+          uu$$$$$$$$$$$$$$$$$uu
+         u$$$$$$$$$$$$$$$$$$$$$u
+        u$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$"   "$$$"   "$$$$$$u
+       "$$$$"      u$u       $$$$"
+        $$$u       u$u       u$$$
+        $$$u      u$$$u      u$$$
+         "$$$$uu$$$   $$$uu$$$$"
+          "$$$$$$$"   "$$$$$$$"
+            u$$$$$$$u$$$$$$$u
+             u$"$"$"$"$"$"$u
+  uuu        $$u$ $ $ $ $u$$       uuu
+ u$$$$        $$$$$u$u$u$$$       u$$$$
+  $$$$$uu      "$$$$$$$$$"     uu$$$$$$
+u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$
+$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"
+ """      ""$$$$$$$$$$$uu ""$"""
+           uuuu ""$$$$$$$$$$uuu
+  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
+  $$$$$$$$$$""""           ""$$$$$$$$$$$"
+   "$$$$$"                      ""$$$$""
+     $$$"                         $$$$"
+EOF
+;;
+"2" | "3")
+cat << "EOF"
+          .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+EOF
+;;
+esac
+
