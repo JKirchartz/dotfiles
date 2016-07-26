76e86498349121b532757aeb3c4fbe79f7ae5dd3
Joel Kirchartz <jkirchartz@gmail.com>
2016-07-26 17:23:37 -0400

    still trying to get this rootsinit.sh to work properly... somehow made it worse removing subshells

--- scripts/rootsinit.sh
+++ scripts/rootsinit.sh
@@ -2,61 +2,64 @@
 #
 # rootsinit.sh
 #
-# Copyleft (ↄ) 2016 joel <me@jkirchartz.com>
-#
-# Distributed under terms of the NPL (Necessary Public License) license.
+# 2016 Joel Kirchartz <joel@firemancreative.com>
 #
 
 if [ $# -eq 0 ]; then
   echo "please provide a domain name for this project"
-  echo "$> rootsinit.sh example.com"
+  echo "$> rootsinit.sh example.dev"
   exit;
 fi
 
+DOMAIN="$1"
+THEME="${DOMAIN%.*}"
 
 echo "Creating project directory:"
-mkdir $1
-cd $1
-(
+mkdir $DOMAIN
+
 echo "Clone Trellis:"
-git clone --depth=1 git@github.com:roots/trellis.git
-rm -rf trellis/.git
+git clone --depth=1 git@github.com:roots/trellis.git "$DOMAIN/trellis"
+rm -rf "$DOMAIN/trellis/.git"
+
 echo "Cloning Bedrock:"
 git clone --depth=1 git@github.com:roots/bedrock.git site
 rm -rf site/.git
+
 echo "Clone Sage:"
-git clone https://github.com/roots/sage.git site/web/app/themes/sage
-rm -rf site/web/app/themes/sage/.git
-rm -rf site/web/app/themes/sage/.github
-)
+git clone https://github.com/roots/sage.git "$DOMAIN/site/web/app/themes/$THEME"
+rm -rf "$DOMAIN/site/web/app/themes/$THEME/.git"
+rm -rf "$DOMAIN/site/web/app/themes/$THEME/.github"
+
+echo "Installing the Ansible Galaxy roles:"
+cd "$PWD/$DOMAIN/trellis"
+ansible-galaxy install -r requirements.yml
+
 echo "Installing JS packages"
-(
-cd $1/site/web/app/themes/sage
+cd "$PWD/$DOMAIN/site/web/app/themes/$THEME"
 npm install
 bower install
-)
-echo "Installing the Ansible Galaxy roles:"
-(
-cd $1/trellis
-ansible-galaxy install -r requirements.yml
-)
+
+cd "$PWD/$DOMAIN"
 
 echo "Creating shortcut to theme"
-ln -s site/web/app/themes/sage theme
+ln -s "$PWD/$DOMAIN/site/web/app/themes/sage" theme
 
-files[1]="$1/site/web/app/themes/sage/assets/manifest.json"
-files[2]="$1/trellis/group_vars/development/mail.yml"
-files[3]="$1/trellis/group_vars/development/wordpress_sites.yml"
+files[1]="$DOMAIN/site/web/app/themes/$THEME/assets/manifest.json"
+files[2]="$DOMAIN/trellis/group_vars/development/mail.yml"
+files[3]="$DOMAIN/trellis/group_vars/development/wordpress_sites.yml"
 
 # `git grep "example.com" | cut -d':' -f'1'`
 for file in "${files[@]}"
 do
-  echo $1
+  echo $DOMAIN
   echo $file
-  sed -i '' -e "s/example.dev/$1/g" $file
+  if [ -f $file ]; then
+    echo 'exists'
+  fi
+  sed -i '' -e "s/example.dev/$DOMAIN/g" $file
 done
 
 git init
 git add .
-git commit -am "Initial commit for $1"
+git commit -am "Initial commit for $DOMAIN"
 
