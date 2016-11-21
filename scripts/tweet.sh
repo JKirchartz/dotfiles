39af4fc4b04378ccc60eade289433f2c03d6d451
Joel Kirchartz <jkirchartz@gmail.com>
2016-11-21 19:05:48 +0100

    fix fortune scripts for IRC

--- scripts/tweet.sh
+++ scripts/tweet.sh
@@ -12,4 +12,4 @@
 
 fortunedir=$(fortune -f 2>&1 | head -n1 | cut -d' ' -f2)
 
-fortune -ae "$fortunedir" "$HOME/dotfiles/scripts/fortune/" | tr '(\t|\r\n)' ' ' |  tr -s '[:blank:]'
+fortune -ae -n 139 "$fortunedir" "$HOME/dotfiles/scripts/fortune/" | tr '(\t|\r\n)' ' ' |  tr -s '[:blank:]'
