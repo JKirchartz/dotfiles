7eb219c46fb382c35eb63ae59a6461e0afd92ebd
Joel Kirchartz <jkirchartz@gmail.com>
2017-02-27 23:29:13 +0100

    add provisioning script, update plug.vim, and IDK what fortune's stubtree was doing

---
+++ provision.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+LABEL=`uname -a`
+
+case $LABEL in
+	*arch*) sudo pacman -S git vim tmux fortune-mod cowsay;;
+        *debian*|*ubuntu*) sudo apt-get install git vim tmux fortune cowsay;;
+        *darwin*) brew install git vim tmux fortune cowsay;;
+esac
