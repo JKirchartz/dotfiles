c8858b391d893479e24ac5917d4f075b3596d533
Joel Kirchartz <jkirchartz@gmail.com>
2017-03-27 12:03:47 -0400

    save stuff for later

---
+++ scripts/wave.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# wave.sh
+# Copyright (C) 2017 jkirchartz <me@jkirchartz.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+
+
+
+while true;do
+echo -n '\E[32;40mo/^H' ; sleep 0.05
+echo -n '\E[32;40mo7^H' ; sleep 0.05
+done
