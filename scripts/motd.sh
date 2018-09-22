2447b04b1ee279017eb5e056fde7cb64857d1932
Joel Kirchartz <jkirchartz@gmail.com>
2018-09-22 02:29:58 +0200

    update motd.sh, etc

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -7,6 +7,8 @@
 # Distributed under terms of the NPL license.
 #
 
+date +"Good %H, $USER" | sed -e 's/0[5-9]\|1[01]/Morning/' -e 's/1[2-8]/Afternoon/' -e 's/[0-9]\+/Evening/'
+
 if [ -z "${VIMRUNTIME}" ]; then
   if which fortune > /dev/null; then
     if which cowsay > /dev/null; then
