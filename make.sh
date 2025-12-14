20e3b64b353d10d99d0a11ef5bbdd438206d498c
JKirchartz <jkirchartz@gmail.com>
2025-12-14 11:14:00 -0500

    clean up make while we're breaking everything...

--- make.sh
+++ make.sh
@@ -3,73 +3,14 @@
 # .make.sh
 # On *nix this script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 # Cygwin/msys will have ~/dotfiles copied into the home directory
-# this is basically a replacement for GNU stow... go figure
+# this is basically a dumber replacement for GNU stow... go figure
 ############################
-
-#########
-# Variables
-###
 
 dir=$HOME/dotfiles                    # dotfiles directory
 olddir=$HOME/dotfiles_old             # old dotfiles backup directory
-case "$OSTYPE" in
-    msys|cygwin)
-        # Files for cygwin & msys << obsoleted by windows 10 anniversary release
-        echo "Preparing files for *doze"
-        files="bashrc bash_profile profile ssh jshintrc eslint.json"
-        ;;
-    *)
-        # Files for *nix
-        echo "Preparing files for *nix"
-        files="bashrc bash_profile profile profile ssh jshintrc eslint.json"
-        ;;
-esac
 
-
-##########
-# create dotfiles_old in homedir
-###
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-#########
-# change to the dotfiles directory
-###
-echo "Changing to the $dir directory"
-cd $dir || exit
-echo "...done"
-
-case $OSTYPE in
-    msys|cygwin)
-        # move any existing dotfiles in homedir to dotfiles_old directory, then copy dotfiles
-        for file in $files; do
-            echo "Moving existing $file from ~ to $olddir"
-            [ -f "$HOME/.$file" ] && mv "$HOME/.$file" "$HOME/dotfiles_old/"
-            echo "Creating copying to $file in home directory."
-            cp -r "$dir/$file" "$HOME/.$file"
-        done
-        ;;
-    *)
-        # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-        for file in $files; do
-            echo "Moving existing $file from ~ to $olddir"
-            [ -f "$HOME/.$file" ] && mv "$HOME/.$file" "$HOME/dotfiles_old/"
-            echo "Creating symlink to $file in home directory."
-            ln -s "$dir/$file" "$HOME/.$file"
-        done
-	for file in config/*/; do
-            echo "Moving existing $file from ~ to $olddir"
-            [ -f "$file" ] && mv "$file" "$HOME/dotfiles_old/"
-            echo "Creating symlink to $file in home directory."
-            ln -s "$dir/$file" "$file"
-	done
-        ;;
-    esac
-
-if [[ $files == *"ssh"* ]]; then
-        cp "$HOME/dotfiles_old/.ssh/*" "$HOME/.ssh/"
-fi
+# this is a list of files that need moved to the home directory
+files="bashrc bash_profile profile profile ssh jshintrc eslint.json"
 
 ##########
 # checkout submodules (the recursiveneess should setup vim/etc for us, too!
@@ -119,3 +60,40 @@ echo "$fortunes"
 #   echo "Installing zsh plugins via ZPlug"
 #   zplug install
 # fi
+
+
+##########
+# create dotfiles_old in homedir
+###
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+#########
+# change to the dotfiles directory
+###
+echo "Changing to the $dir directory"
+cd $dir || exit
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving existing $file from ~ to $olddir"
+    [ -f "$HOME/.$file" ] && mv "$HOME/.$file" "$HOME/dotfiles_old/"
+    echo "Creating symlink to $file in home directory."
+    ln -s "$dir/$file" "$HOME/.$file"
+done
+
+# now lets do the same thing for folders in the ./config/ directory
+for file in ./config/*/; do
+    echo "Moving existing $file from ~/.config to $olddir"
+    [ -f "$file" ] && mv "$file" "$HOME/dotfiles_old/"
+    echo "Creating symlink from $file to $dir/$file"
+    # TODO: uncomment last line after this check...
+    # ln -s "$file" "$dir/$file"
+done
+
+if [[ $files == *"ssh"* ]]; then
+        cp "$HOME/dotfiles_old/.ssh/*" "$HOME/.ssh/"
+fi
+
